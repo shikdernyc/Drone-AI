d4156bf4d044ae076b1791a407f4341257c3c898
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-05-13 10:11:40 -0400

    Added Battery Cost for each types of nodes

--- student/config.py
+++ student/config.py
@@ -24,14 +24,14 @@ configuration = {
         "id":  "desert",
         "marker": 'D',
         "attributes":
-             {"agent": None, "cost": 1}
+             {"agent": None, "cost": 1, "battery": 1}
         },
     "desert-traversed": {
         "img": "game/graphics/terrains/desertTraversed100.png",
         "id":  "desert-traversed",
         "marker": 'd',
         "attributes":
-             {"agent": None, "cost": 1}
+             {"agent": None, "cost": 1, "battery": 1}
         },
     "plains": {
         "img": "game/graphics/terrains/plains100.png",
@@ -39,7 +39,7 @@ configuration = {
         "marker": 'P',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1}
+             {"agent": None, "cost": 1, "battery": 1}
         },
     "plains-traversed": {
         "img": "game/graphics/terrains/plainsTraversed100.png",
@@ -47,7 +47,7 @@ configuration = {
         "marker": 'p',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1}
+             {"agent": None, "cost": 1, "battery": 1}
         },
     "hills": {
         "img": "game/graphics/terrains/hills100.png",
@@ -55,7 +55,7 @@ configuration = {
         "marker": 'H',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 1},
         },
     "hills-traversed": {
         "img": "game/graphics/terrains/hillsTraversed100.png",
@@ -63,7 +63,7 @@ configuration = {
         "marker": 'h',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 1},
         },
     "forest": {
         "img": "game/graphics/terrains/forest100.png",
@@ -71,7 +71,7 @@ configuration = {
         "marker": 'F',
         "num": 5,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 1},
         },
     "forest-traversed": {
         "img": "game/graphics/terrains/forestTraversed100.png",
@@ -79,7 +79,7 @@ configuration = {
         "marker": 'f',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 1},
         },
     "sea": {
         "img": "game/graphics/terrains/sea100.png",
@@ -87,7 +87,7 @@ configuration = {
         "marker": 'S',
         "num": 1,
         "attributes":
-             {"agent": None, "blocked": True}
+             {"agent": None, "blocked": True, "battery": 1}
         },
     "sea-traversed": {
         "img": "game/graphics/terrains/seaTraversed100.png",
@@ -95,7 +95,7 @@ configuration = {
         "marker": 's',
         "num": 0,
         "attributes":
-             {"agent": None, "blocked": True}
+             {"agent": None, "blocked": True, "battery": 1}
         },
     "goal": {
         "img": "game/graphics/locations/camera100.png",
@@ -103,7 +103,7 @@ configuration = {
         "marker": 'G',
         "num": 4,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 1},
         },
     "goal-traversed": {
         "img": "game/graphics/locations/cameraTraversed100.png",
@@ -111,7 +111,7 @@ configuration = {
         "marker": 'g',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 1},
         },
     "drone-base": {
         "img": "game/graphics/locations/droneBase100.png",
@@ -119,7 +119,7 @@ configuration = {
         "marker": 'B',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 0},
         },
     "drone-base-traversed": {
         "img": "game/graphics/locations/droneBaseTraversed100.png",
@@ -127,7 +127,7 @@ configuration = {
         "marker": 'b',
         "num": 0,
         "attributes":
-             {"agent": None, "cost": 1},
+             {"agent": None, "cost": 1, "battery": 0},
         }
      }
 }
